755c4e43009194e08de24bcaa07f771e9f47fa4a
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-07-24 12:29:18 +0530

    final project vue

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Trade or View your Portfolio</h1>
+    <h4>You may Save & Load your Data</h4>
+    <h4>Click on 'End Day' to begin a New Day!</h4>
+    <hr />
+    <p>Your Funds : {{ funds | currency }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: "Lato", cursive, sans-serif;
+}
+
+p {
+  font-family: "Ranchers", cursive, sans-serif;
+  font-size: 150%;
+  color: rgba(0, 0, 0, 0.774);
+}
+</style>
